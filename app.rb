7cb2905edc768759aab0eaa9cdfb45791fcce328
Alec.Murphy <alecmurphypersonal@gmail.com>
2018-09-05 16:57:17 -0400

    sign-up/in finished

--- app.rb
+++ app.rb
@@ -9,23 +9,25 @@ enable :sessions
 
 get '/' do
     @title= 'Home'
-    erb :home, layout: :layout
+    
     if session[:user_id] == nil
-        redirect '/sign-in'
+        return redirect '/sign-in'
     elsif session[:user_id]
         @user = current_user
     end
+
+    erb :home
 end
 
 get '/sign-in' do
     @title = 'Sign In'
-    erb :sign_in, layout: :layout
+    erb :sign_in
 
 end
 
 get '/sign-up' do
     @title = 'Sign Up'
-    erb :sign_up, layout: :layout
+    erb :sign_up
 end
 
 get '/sign-out' do 
@@ -47,22 +49,23 @@ post '/sign-up' do
 
 end
 
+post '/sign-in' do
+
+    user = User.where(username: params[:user][:username]).first
+
+    if user && user.username == params[:user][:username] && user.password == params[:user][:password]
+        session[:user_id] = user.id
+        
+        return redirect '/'
+    else
+        flash[:failure] = "Incorrect Username or Password."
+        redirect '/sign-in'
+    end
+end
+
 def current_user
     if session[:user_id]
         User.find(session[:user_id])
     end
 end
 
-
-# post '/sign-in' do
-#     user = User.where(email: params[:email]).first
-
-#     if user && user.password == params[:password]
-#         session[:user_id] = user.id
-        
-#         redirect '/'
-#     else
-#         redirect '/sign-in'
-#         flash[:failure] = "Incorrect Username or Password."
-#     end
-# end
